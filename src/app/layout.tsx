e8b4c4c7674fd45fb8554a2b5bf3fc6e90131baa
tooba45 <toobanaz089@gmail.com>
2024-11-28 00:18:34 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Header/> */}
+      
         {children}
-        {/* <Footer/> */}
+        
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hero from "./components/hero";
 import ExploreCourses from "./components/explorecourses";
 import Header from "./components/header";
